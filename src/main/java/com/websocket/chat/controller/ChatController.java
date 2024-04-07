90d4ab155179c1849bc2d03d5228ebbdd93fa309
Siva Kumar Gangainathan <siva.kumar.gangainathan@walmart.com>
2024-04-07 08:16:14 +0530

    first commit

---
+++ src/main/java/com/websocket/chat/controller/ChatController.java
@@ -0,0 +1,29 @@
+package com.websocket.chat.controller;
+
+import com.websocket.chat.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Controller
+public class ChatController {
+
+    @Autowired(required = true)
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/message")
+    @SendTo("/chatroom/public")
+    public Message receivePublicMessage(@Payload Message message) {
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message);
+        return message;
+    }
+}
